d8e72424a1a3974b91e6d26217541790e8afdae8
jaemin-Yoo <iamjm29@naver.com>
2022-02-14 11:33:57 +0900

    Add checkBox Function

--- app/src/main/java/com/infinity/omos/RegisterNickActivity.kt
+++ app/src/main/java/com/infinity/omos/RegisterNickActivity.kt
@@ -1,13 +1,21 @@
 package com.infinity.omos
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.infinity.omos.databinding.ActivityRegisterBinding
 import com.infinity.omos.databinding.ActivityRegisterNickBinding
 import com.infinity.omos.viewmodels.RegisterNickViewModel
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register_nick.*
 
 class RegisterNickActivity : AppCompatActivity() {
@@ -21,10 +29,58 @@ class RegisterNickActivity : AppCompatActivity() {
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        // 메인 페이지 이동
+        // 이용약관 체크박스 클릭 시
+        viewModel.checkBoxTos.observe(this, Observer { state ->
+            state?.let {
+                if (it){
+                    checkbox_tos.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.orange))
+                } else{
+                    checkbox_tos.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.light_gray))
+                }
+            }
+        })
+
+        // 개인정보 보호정책 체크박스 클릭 시
+        viewModel.checkBoxPP.observe(this, Observer { state ->
+            state?.let {
+                if (it){
+                    checkbox_pp.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.orange))
+                } else{
+                    checkbox_pp.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.light_gray))
+                }
+            }
+        })
+
+        // 완료 버튼 활성화
+        viewModel.allState.observe(this, Observer { state ->
+            state?.let {
+                if (it){
+                    btn_complete.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.orange))
+                    btn_complete.setTextColor(ContextCompat.getColor(this, R.color.white))
+                    btn_complete.isEnabled = true
+                } else{
+                    btn_complete.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.light_gray))
+                    btn_complete.setTextColor(ContextCompat.getColor(this, R.color.dark_gray))
+                    btn_complete.isEnabled = false
+                }
+            }
+        })
+
+        // 로그인 페이지 이동
         btn_complete.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
+            var intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
+        }
+
+        // 이용약관 보기
+        btn_look_tos.setOnClickListener {
+            Toast.makeText(this, resources.getString(R.string._ing), Toast.LENGTH_SHORT).show()
+        }
+
+        // 개인정보 보호정책 보기
+        btn_look_pp.setOnClickListener {
+            Toast.makeText(this, resources.getString(R.string._ing), Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/infinity/omos/viewmodels/RegisterNickViewModel.kt
+++ app/src/main/java/com/infinity/omos/viewmodels/RegisterNickViewModel.kt
@@ -2,7 +2,30 @@ package com.infinity.omos.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class RegisterNickViewModel(application: Application): AndroidViewModel(application) {
+    var checkBoxTos = MutableLiveData<Boolean>()
+    var checkBoxPP = MutableLiveData<Boolean>()
+    var allState = MutableLiveData<Boolean>()
 
+    init {
+        checkBoxTos.value = false
+        checkBoxPP.value = false
+        allState.value = false
+    }
+
+    fun checkBoxTosClick(){
+        checkBoxTos.value = checkBoxTos.value != true
+        checkState()
+    }
+
+    fun checkBoxPPClick(){
+        checkBoxPP.value = checkBoxPP.value != true
+        checkState()
+    }
+
+    private fun checkState(){
+        allState.value = checkBoxTos.value == true && checkBoxPP.value == true
+    }
 }
