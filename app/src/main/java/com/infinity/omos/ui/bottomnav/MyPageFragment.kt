7e4a6128aaa716517052dea3c84967708a53c95c
jaemin-Yoo <iamjm29@naver.com>
2022-02-21 12:40:18 +0900

    Add Logout Function

--- app/src/main/java/com/infinity/omos/ui/bottomnav/MyPageFragment.kt
+++ app/src/main/java/com/infinity/omos/ui/bottomnav/MyPageFragment.kt
@@ -1,60 +1,48 @@
 package com.infinity.omos.ui.bottomnav
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.infinity.omos.R
+import com.infinity.omos.ui.onboarding.LoginActivity
+import com.infinity.omos.utils.GlobalApplication
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.android.synthetic.main.fragment_my_page.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_logout.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null){
+                    Toast.makeText(context, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(context, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+                    GlobalApplication.prefs.setString("accessToken", null)
+                    GlobalApplication.prefs.setString("refreshToken", null)
+                    GlobalApplication.prefs.setLong("userId", -1L)
+                    val intent = Intent(context, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
                 }
             }
+        }
     }
 }
