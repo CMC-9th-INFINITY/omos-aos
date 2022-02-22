79640c38cb4b0298a689fb6d89d15ca52adac8de
jaemin-Yoo <iamjm29@naver.com>
2022-02-22 18:20:17 +0900

    Add Category Toolbar

---
+++ app/src/main/java/com/infinity/omos/SelectCategoryActivity.kt
@@ -0,0 +1,43 @@
+package com.infinity.omos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register_nick.*
+
+class SelectCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_category)
+
+        initToolBar()
+    }
+
+    private fun initToolBar(){
+        toolbar.title = ""
+        setSupportActionBar(toolbar) // 툴바 사용
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.appbar_action_next, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_next -> {
+                Toast.makeText(this, "다음", Toast.LENGTH_SHORT).show()
+                true
+            }
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
--- app/src/main/java/com/infinity/omos/ui/bottomnav/TodayFragment.kt
+++ app/src/main/java/com/infinity/omos/ui/bottomnav/TodayFragment.kt
@@ -1,21 +1,16 @@
 package com.infinity.omos.ui.bottomnav
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.infinity.omos.R
+import com.infinity.omos.SelectCategoryActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_today.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TodayFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TodayFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +23,14 @@ class TodayFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_today, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_select.setOnClickListener {
+            val intent = Intent(context, SelectCategoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
