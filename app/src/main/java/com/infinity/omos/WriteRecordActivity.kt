51ef71681b4476aaebca9ff90ae736f7e14ba464
jaemin-Yoo <iamjm29@naver.com>
2022-02-23 15:01:26 +0900

    Add image in WriteRecord

--- app/src/main/java/com/infinity/omos/WriteRecordActivity.kt
+++ app/src/main/java/com/infinity/omos/WriteRecordActivity.kt
@@ -1,20 +1,34 @@
 package com.infinity.omos
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.infinity.omos.databinding.ActivitySelectCategoryBinding
 import com.infinity.omos.databinding.ActivityWriteRecordBinding
 import com.infinity.omos.viewmodels.SelectCategoryViewModel
 import com.infinity.omos.viewmodels.WriteRecordViewModel
 import kotlinx.android.synthetic.main.activity_register_nick.*
+import kotlinx.android.synthetic.main.activity_register_nick.toolbar
+import kotlinx.android.synthetic.main.activity_write_record.*
 
 class WriteRecordActivity : AppCompatActivity() {
 
+    private lateinit var result: ActivityResultLauncher<Intent>
     private val viewModel: WriteRecordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +39,41 @@ class WriteRecordActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
         initToolBar()
+
+        btn_gallery.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            result.launch(intent)
+        }
+
+        result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == RESULT_OK){
+                var currentImageUri = it.data?.data
+                try {
+                    currentImageUri?.let {
+                        if(Build.VERSION.SDK_INT < 28) {
+                            val bitmap = MediaStore.Images.Media.getBitmap(
+                                this.contentResolver,
+                                currentImageUri
+                            )
+                            val bd = BitmapDrawable(resources, bitmap)
+                            img_record_title.setImageDrawable(bd)
+                        } else {
+                            val source = ImageDecoder.createSource(this.contentResolver, currentImageUri)
+                            val bitmap = ImageDecoder.decodeBitmap(source)
+                            val bd = BitmapDrawable(resources, bitmap)
+                            img_record_title.setImageDrawable(bd)
+                        }
+                    }
+                }catch(e:Exception) {
+                    e.printStackTrace()
+                }
+            } else if(it.resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "취소", Toast.LENGTH_LONG).show()
+            } else{
+                Log.d("WriteRecord","error")
+            }
+        }
     }
 
     private fun initToolBar(){
--- app/src/main/java/com/infinity/omos/viewmodels/WriteRecordViewModel.kt
+++ app/src/main/java/com/infinity/omos/viewmodels/WriteRecordViewModel.kt
@@ -1,8 +1,20 @@
 package com.infinity.omos.viewmodels
 
 import android.app.Application
+import android.widget.EditText
 import androidx.lifecycle.AndroidViewModel
 
 class WriteRecordViewModel(application: Application): AndroidViewModel(application) {
+    var prevText = ""
 
+    fun setPrevText(et: EditText){
+        prevText = et.text.toString()
+    }
+
+    fun limitText(et: EditText){
+        if (et.lineCount > 2){
+            et.setText(prevText)
+            et.setSelection(et.length())
+        }
+    }
 }
