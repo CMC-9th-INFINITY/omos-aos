635f2b1b5019f724883ebf0dd6ced9598c80ea00
jaemin-Yoo <iamjm29@naver.com>
2022-03-04 00:58:33 +0900

    RoundedImageView

---
+++ app/src/main/java/com/infinity/omos/utils/RoundImageView.kt
@@ -0,0 +1,54 @@
+package com.infinity.omos.utils
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.widget.ImageView
+
+
+class RoundImageView : androidx.appcompat.widget.AppCompatImageView {
+    private val roundRect = RectF()
+    private val maskPaint = Paint()
+    private val zonePaint = Paint()
+    private var rectRadius = 7f
+
+
+    constructor(context: Context?) : super(context!!) {
+        initView()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {
+        initView()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {
+        initView()
+    }
+
+    private fun initView() {
+        maskPaint.isAntiAlias = true
+        maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        zonePaint.isAntiAlias = true
+        zonePaint.color = Color.WHITE
+        val density = resources.displayMetrics.density
+        rectRadius *= density
+    }
+
+    fun setRectRadius(radius: Float) {
+        rectRadius = radius
+        invalidate()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        roundRect.set(0f, 0f, width.toFloat() - rectRadius, height.toFloat() - rectRadius)
+    }
+
+    override fun draw(canvas: Canvas?) {
+        canvas?.saveLayer(roundRect, zonePaint, Canvas.ALL_SAVE_FLAG)
+        canvas?.drawRoundRect(roundRect, rectRadius, rectRadius, zonePaint)
+        canvas?.saveLayer(roundRect, maskPaint, Canvas.ALL_SAVE_FLAG)
+        super.draw(canvas)
+        canvas?.restore()
+    }
+}
