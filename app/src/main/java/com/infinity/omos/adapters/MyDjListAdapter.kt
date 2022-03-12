d4e012ad14c1f4c5cebcb60d0b09a213af09956e
jaemin-Yoo <iamjm29@gmail.com>
2022-03-13 02:21:36 +0900

    Apply Border in DJ

--- app/src/main/java/com/infinity/omos/adapters/MyDjListAdapter.kt
+++ app/src/main/java/com/infinity/omos/adapters/MyDjListAdapter.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.infinity.omos.data.MyDj
-import com.infinity.omos.data.Profile
-import com.infinity.omos.data.Record
-import com.infinity.omos.data.SaveRecord
+import com.infinity.omos.R
+import com.infinity.omos.data.*
 import com.infinity.omos.databinding.ListMydjItemBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MyDjListAdapter internal constructor(context: Context):
     ListAdapter<Profile, MyDjListAdapter.ViewHolder>(
@@ -21,6 +21,9 @@ class MyDjListAdapter internal constructor(context: Context):
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
     private val context = context
+
+    private var isChecked = ArrayList<Boolean>()
+    private var prevChecked = 0
 
     private lateinit var itemClickListener: OnItemClickListener
 
@@ -43,17 +46,34 @@ class MyDjListAdapter internal constructor(context: Context):
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val dj = getItem(position)
-        holder.bind(dj)
+        holder.bind(dj, position)
     }
 
     inner class ViewHolder(private val binding: ListMydjItemBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(dj: Profile) {
+        fun bind(dj: Profile, num: Int) {
             binding.dj = dj
             binding.executePendingBindings() //데이터가 수정되면 즉각 바인딩
+
+            if (num >= isChecked.size){
+                isChecked.add(num, false)
+            }
+
+            if (isChecked[num]){
+                binding.imgAlbumCover.borderColor = ContextCompat.getColor(context, R.color.orange)
+            } else{
+                binding.imgAlbumCover.borderColor = ContextCompat.getColor(context, R.color.dark)
+            }
 
             val pos = adapterPosition
             if (pos != RecyclerView.NO_POSITION){
                 itemView.setOnClickListener {
+                    // ViewHolder 재사용 방지
+                    if (!isChecked[num]){
+                        isChecked[prevChecked] = false
+                        isChecked[num] = true
+                        prevChecked = num
+                        notifyDataSetChanged() // 이미지 재적용
+                    }
                     itemClickListener.onClick(itemView, pos, dj.userId)
                 }
             }
