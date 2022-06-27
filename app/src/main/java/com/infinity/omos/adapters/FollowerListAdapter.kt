549786e7f6612512c1e44c0673328518e348dda1
jaemin-Yoo <iamjm29@naver.com>
2022-06-28 02:30:18 +0900

    Feat: 팔로우 목록 ListAdapter Setup

---
+++ app/src/main/java/com/infinity/omos/adapters/FollowerListAdapter.kt
@@ -0,0 +1,65 @@
+package com.infinity.omos.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.infinity.omos.data.Profile
+import com.infinity.omos.databinding.ListFollowerItemBinding
+
+class FollowerListAdapter : ListAdapter<Profile, RecyclerView.ViewHolder>(FollowerDiffCallback()) {
+    private lateinit var itemClickListener: OnItemClickListener
+
+    interface OnItemClickListener{
+        fun onClick(dj: Profile)
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener){
+        this.itemClickListener = onItemClickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return FollowerViewHolder(
+            ListFollowerItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val dj = getItem(position)
+        (holder as FollowerViewHolder).bind(dj)
+    }
+
+    inner class FollowerViewHolder(
+        private val binding: ListFollowerItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener {
+                binding.dj?.let { dj ->
+                    itemClickListener.onClick(dj)
+                }
+            }
+        }
+
+        fun bind(item: Profile) {
+            binding.apply {
+                dj = item
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+private class FollowerDiffCallback : DiffUtil.ItemCallback<Profile>() {
+    override fun areItemsTheSame(oldItem: Profile, newItem: Profile): Boolean {
+        return oldItem.userId == newItem.userId
+    }
+
+    override fun areContentsTheSame(oldItem: Profile, newItem: Profile): Boolean {
+        return oldItem == newItem
+    }
+}
