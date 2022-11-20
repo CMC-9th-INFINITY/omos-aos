718faa12772260a9b8fe4758768797bfae297f47
jaemin-Yoo <iamjm29@gmail.com>
2022-11-20 23:07:06 +0900

    feat: Navigation component 적용

---
+++ app/src/main/java/com/infinity/omos/FakeMainActivity.kt
@@ -0,0 +1,15 @@
+package com.infinity.omos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * onboarding Navigation component 적용을 위한 Activity
+ * 추후 MainActivity로 변경 예정
+ */
+class FakeMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_main)
+    }
+}
