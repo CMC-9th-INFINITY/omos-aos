248fcec6f5343b908c59bc37a0204524c5669418
jaemin-Yoo <iamjm29@gmail.com>
2022-11-21 13:37:32 +0900

    Design: 비밀번호 찾기 UI 구성

--- app/src/main/java/com/infinity/omos/ui/onboarding/LoginFragment.kt
+++ app/src/main/java/com/infinity/omos/ui/onboarding/LoginFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.infinity.omos.R
+import com.infinity.omos.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvFindPw.setOnClickListener {
+            val directions =
+                LoginFragmentDirections.actionLoginFragmentToFindPwFragment()
+            findNavController().navigate(directions)
+        }
+    }
 }
