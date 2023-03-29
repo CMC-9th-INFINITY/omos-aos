b0b7e466077209744617f79ee2ed4a70955f8b48
jaemin-Yoo <iamjm29@naver.com>
2023-03-29 21:09:14 +0900

    Feat: 회원가입 화면 기능 구현 (#13)
    - 유효성 검사
    - 이메일 인증

--- app/src/main/java/com/infinity/omos/ui/onboarding/signup/SignUpFragment.kt
+++ app/src/main/java/com/infinity/omos/ui/onboarding/signup/SignUpFragment.kt
@@ -1,13 +1,22 @@
 package com.infinity.omos.ui.onboarding.signup
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.infinity.omos.R
 import com.infinity.omos.databinding.FragmentSignUpBinding
+import com.infinity.omos.ui.onboarding.base.AuthCodeState
+import com.infinity.omos.ui.onboarding.base.OnboardingState
+import com.infinity.omos.ui.onboarding.base.OnboardingViewModel
+import com.infinity.omos.ui.view.OmosDialog
+import com.infinity.omos.utils.repeatOnStarted
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpFragment : Fragment() {
 
     private lateinit var binding: FragmentSignUpBinding
@@ -22,5 +31,182 @@ class SignUpFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+        collectData()
+    }
+
+    private fun initListener() {
+        initEmailListener()
+        initAuthCodeListener()
+        initPasswordListener()
+        initConfirmPasswordListener()
+        initButtonClickListener()
+    }
+
+    private fun initEmailListener() = with(binding.ofvEmail) {
+        setOnTextChangeListener { email ->
+            viewModel.setEmail(email)
+        }
+
+        setOnFocusChangeListener { hasFocus ->
+            viewModel.checkEmailValidation(hasFocus)
+        }
+    }
+
+    private fun initAuthCodeListener() = with(binding.ofvEmailAuthCode) {
+        setOnTextChangeListener { text ->
+            viewModel.setAuthCode(text)
+            viewModel.checkAuthCodeValidation(resources.getInteger(R.integer.auth_code_length))
+            viewModel.changeCompleteState()
+        }
+
+        setOnFocusChangeListener {
+            viewModel.checkAuthCodeValidation(resources.getInteger(R.integer.auth_code_length))
+        }
+    }
+
+    private fun initPasswordListener() = with(binding.ofvPassword) {
+        setOnTextChangeListener { text ->
+            viewModel.setPassword(text)
+            viewModel.changeCompleteState()
+
+            if (viewModel.confirmPassword.value.isNotEmpty()) {
+                viewModel.checkConfirmPasswordValidation()
+            }
+        }
+
+        setOnFocusChangeListener { hasFocus ->
+            viewModel.checkPasswordValidation(hasFocus)
+        }
+
+        setOnPasswordToggleClickListener {
+            viewModel.changePasswordVisibleState()
+        }
+    }
+
+    private fun initConfirmPasswordListener() = with(binding.ofvConfirmPassword) {
+        setOnTextChangeListener { text ->
+            viewModel.setConfirmPassword(text)
+            viewModel.changeCompleteState()
+        }
+
+        setOnFocusChangeListener { hasFocus ->
+            viewModel.checkConfirmPasswordValidation(hasFocus)
+        }
+
+        setOnPasswordToggleClickListener {
+            viewModel.changeConfirmPasswordVisibleState()
+        }
+    }
+    private fun initButtonClickListener() {
+        binding.tvSendAuthMail.setOnClickListener {
+            showAlertDialog()
+        }
+
+        binding.btnNext.setOnClickListener {
+            // TODO: 닉네임 입력 페이지로 이동
+        }
+    }
+
+    private fun collectData() {
+        collectFieldViewError()
+        collectFieldViewPasswordState()
+        collectEvent()
+    }
+
+    private fun collectFieldViewError() {
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.errorEmail.collect { (state, msg) ->
+                binding.ofvEmail.setShowErrorMsg(state, msg)
+            }
+        }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.errorAuthCode.collect { (state, msg) ->
+                binding.ofvEmailAuthCode.setShowErrorMsg(state, msg)
+            }
+        }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.errorPassword.collect { (state, msg) ->
+                binding.ofvPassword.setShowErrorMsg(state, msg)
+            }
+        }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.errorConfirmPassword.collect { (state, msg) ->
+                binding.ofvConfirmPassword.setShowErrorMsg(state, msg)
+            }
+        }
+    }
+
+    private fun collectFieldViewPasswordState() {
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.isVisiblePassword.collect { isVisiblePassword ->
+                binding.ofvPassword.setShowPassword(isVisiblePassword)
+            }
+        }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.isVisibleConfirmPassword.collect { isVisibleConfirmPassword ->
+                binding.ofvConfirmPassword.setShowPassword(isVisibleConfirmPassword)
+            }
+        }
+    }
+
+    private fun collectEvent() {
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.event.collect { event ->
+                if (event is OnboardingViewModel.Event.ShowDialog) {
+                    showSuccessDialog()
+                    setAgainMail()
+                }
+            }
+        }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.authCodeState.collect { authCodeState ->
+                if (authCodeState is AuthCodeState.Success) {
+                    binding.ofvEmail.setEditTextEnabled(false)
+                    binding.ofvEmailAuthCode.setEditTextEnabled(false)
+                    binding.tvSendAuthMail.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    private fun setAgainMail() {
+        binding.tvSendAuthMail.text = requireContext().getString(R.string.send_again_auth_mail)
+        binding.tvSendAuthMail.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+    }
+
+    private fun showAlertDialog() {
+        val dialog = OmosDialog(requireActivity())
+        dialog.showDialog(
+            title = getString(R.string.alert_send_email),
+            okText = getString(R.string.send),
+            onOkClickListener = {
+                viewModel.checkEmailValidation()
+
+                if (viewModel.errorEmail.value.state.not()) {
+                    viewModel.sendAuthMail()
+                }
+            }
+        )
+    }
+
+    private fun showSuccessDialog() {
+        val dialog = OmosDialog(requireActivity())
+        dialog.showDialog(
+            title = getString(R.string.complete_send_auth_mail),
+            okText = getString(R.string.ok),
+            cancelVisible = false,
+            onOkClickListener = null
+        )
     }
 }
--- app/src/main/java/com/infinity/omos/ui/onboarding/signup/SignUpViewModel.kt
+++ app/src/main/java/com/infinity/omos/ui/onboarding/signup/SignUpViewModel.kt
@@ -1,17 +1,41 @@
 package com.infinity.omos.ui.onboarding.signup
 
-import androidx.lifecycle.ViewModel
-import com.infinity.omos.ui.onboarding.OnboardingState
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import androidx.lifecycle.viewModelScope
+import com.infinity.omos.data.user.UserEmail
+import com.infinity.omos.repository.AuthRepository
+import com.infinity.omos.ui.onboarding.base.AuthCodeState
+import com.infinity.omos.ui.onboarding.base.OnboardingState
+import com.infinity.omos.ui.onboarding.base.OnboardingViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SignUpViewModel : ViewModel() {
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : OnboardingViewModel() {
 
-    private var _isActivatedNext = MutableStateFlow(false)
-    val isActivatedNext = _isActivatedNext.asStateFlow()
+    override fun changeCompleteState() {
+        _isCompleted.value =
+            authCodeState.value is AuthCodeState.Success && validatePasswordWithError().state.not() && password.value == confirmPassword.value
+    }
 
-    private var _state = MutableStateFlow<OnboardingState>(OnboardingState.Nothing)
-    val state = _state.asStateFlow()
-
-
+    fun sendAuthMail() {
+        _authCode.value = ""
+        _state.value = OnboardingState.Loading
+        viewModelScope.launch {
+            val email = UserEmail(email.value)
+            authRepository.sendAuthMail(email)
+                .onSuccess { authCode ->
+                    correctAuthCode = authCode.code
+                    _event.emit(Event.ShowDialog)
+                    _authCodeState.value = AuthCodeState.Ready
+                    _state.value = OnboardingState.Success
+                }
+                .onFailure {
+                    _state.value =
+                        OnboardingState.Failure(OnboardingState.Failure.NETWORK_ERROR_MESSAGE)
+                }
+        }
+    }
 }
