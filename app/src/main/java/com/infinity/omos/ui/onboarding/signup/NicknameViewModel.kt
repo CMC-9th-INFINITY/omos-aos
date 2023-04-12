64dd70fea7aa7d3d61cf6ffb1ac90b70bd30e971
jaemin-Yoo <iamjm29@gmail.com>
2023-04-12 16:43:06 +0900

    Feat: 회원가입 닉네임 화면 기능 구현 (#14)
    - 보기 클릭 시 팝업창 띄우기

--- app/src/main/java/com/infinity/omos/ui/onboarding/signup/NicknameViewModel.kt
+++ app/src/main/java/com/infinity/omos/ui/onboarding/signup/NicknameViewModel.kt
@@ -1,10 +1,40 @@
 package com.infinity.omos.ui.onboarding.signup
 
+import com.infinity.omos.ui.onboarding.ErrorField
 import com.infinity.omos.ui.onboarding.base.OnboardingViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class NicknameViewModel : OnboardingViewModel() {
 
+    private var _nickname = MutableStateFlow("")
+    val nickname = _nickname.asStateFlow()
+
+    private var _tosState = MutableStateFlow(false)
+    val tosState = _tosState.asStateFlow()
+
+    private var _ppState = MutableStateFlow(false)
+    val ppState = _ppState.asStateFlow()
+
+    private var _errorNick = MutableStateFlow(ErrorField(false))
+    val errorNick = _errorNick.asStateFlow()
+
+    fun setTosCheckBox(state: Boolean) {
+        _tosState.value = state
+    }
+
+    fun setPpCheckBox(state: Boolean) {
+        _ppState.value = state
+    }
+
+    fun setNickname(nickname: String) {
+        if (errorNick.value.state) {
+            _errorNick.value = ErrorField(false)
+        }
+        _nickname.value = nickname
+    }
+
     override fun changeCompleteState() {
-        TODO("Not yet implemented")
+        _isCompleted.value = tosState.value && ppState.value
     }
 }
