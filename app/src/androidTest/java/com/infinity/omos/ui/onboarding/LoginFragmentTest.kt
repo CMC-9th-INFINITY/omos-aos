c95d18d1d84b8016154b9c7f763f3ed55d8c9c18
jaemin-Yoo <iamjm29@gmail.com>
2023-04-20 19:55:16 +0900

    Test: 로그인 화면 UI 테스트 구현 (#15)
    - 이메일 양식이 틀렸을 때 에러 메시지가 노출되는가
    - 이메일과 비밀번호가 입력됐을 때 로그인 버튼을 활성화하는가

---
+++ app/src/androidTest/java/com/infinity/omos/ui/onboarding/LoginFragmentTest.kt
@@ -0,0 +1,51 @@
+package com.infinity.omos.ui.onboarding
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.infinity.omos.OnboardingActivity
+import com.infinity.omos.R
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+
+@HiltAndroidTest
+class LoginFragmentTest {
+
+    private val hiltRule = HiltAndroidRule(this)
+    private val activityTestRule = ActivityScenarioRule(OnboardingActivity::class.java)
+
+    @get:Rule
+    val rule: RuleChain = RuleChain
+        .outerRule(hiltRule)
+        .around(activityTestRule)
+
+    @Test
+    fun `check_WrongEmail_ShowErrorMessage`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email"))
+        onView(withId(R.id.ofv_password)).perform(click())
+
+        // then
+        onView(withId(R.id.ofv_email)).check(matches(OmosViewMatchers.isDisplayedErrorMessage()))
+    }
+
+    @Test
+    fun `check_EmailAndPassword_ActivateLoginButton`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email@naver.com"))
+        onView(withId(R.id.ofv_password)).perform(OmosViewActions.replaceText("password"))
+
+        // then
+        onView(withId(R.id.btn_login)).check(matches(isEnabled()))
+    }
+}
