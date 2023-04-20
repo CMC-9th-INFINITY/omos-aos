40611ad8cbd00da98ece095a3c75543854a3a495
jaemin-Yoo <iamjm29@gmail.com>
2023-04-20 19:55:16 +0900

    Test: 로그인 화면 UI 테스트 구현 (#15)
    - 이메일 양식이 틀렸을 때 에러 메시지가 노출되는가
    - 이메일과 비밀번호가 입력됐을 때 로그인 버튼을 활성화하는가

---
+++ app/src/androidTest/java/com/infinity/omos/util/MainTestRunner.kt
@@ -0,0 +1,13 @@
+package com.infinity.omos.util
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class MainTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
