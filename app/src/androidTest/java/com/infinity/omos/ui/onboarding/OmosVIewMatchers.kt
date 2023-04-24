27a8855ef5ad06847e99cd3876337e4a8cb5d4ef
jaemin-Yoo <iamjm29@gmail.com>
2023-04-24 18:08:50 +0900

    Test: 성공 메시지가 나타났는지 확인하는 커스텀 Matcher 추가 (#15)

--- app/src/androidTest/java/com/infinity/omos/ui/onboarding/OmosVIewMatchers.kt
+++ app/src/androidTest/java/com/infinity/omos/ui/onboarding/OmosVIewMatchers.kt
@@ -1,17 +1,35 @@
 package com.infinity.omos.ui.onboarding
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.infinity.omos.R
 import com.infinity.omos.ui.view.OmosFieldView
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
 
 object OmosViewMatchers {
+
     fun isDisplayedErrorMessage(): Matcher<View> {
         return object : TypeSafeMatcher<View>() {
 
             override fun matchesSafely(item: View?): Boolean {
                 return (item as OmosFieldView).error != ErrorMessage.NO_ERROR
+            }
+
+            override fun describeTo(description: Description?) {}
+
+            override fun describeMismatchSafely(item: View?, mismatchDescription: Description?) {
+                super.describeMismatchSafely(item, mismatchDescription)
+            }
+        }
+    }
+
+    fun isDisplayedSuccessMessage(): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+
+            override fun matchesSafely(item: View?): Boolean {
+                return (item as OmosFieldView).binding.tvMsg.currentTextColor == ContextCompat.getColor(item.context, R.color.gray_05)
             }
 
             override fun describeTo(description: Description?) {}
