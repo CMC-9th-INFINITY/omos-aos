833632c435529c7b866821920bf160222c6a4746
jaemin-Yoo <iamjm29@gmail.com>
2023-04-25 17:19:07 +0900

    Test: 회원가입 UI, Unit 테스트 구현 (#15)

---
+++ app/src/androidTest/java/com/infinity/omos/ui/onboarding/SignUpFragmentTest.kt
@@ -0,0 +1,134 @@
+package com.infinity.omos.ui.onboarding
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.infinity.omos.OnboardingActivity
+import com.infinity.omos.R
+import com.infinity.omos.ui.custom.OmosViewActions
+import com.infinity.omos.ui.custom.OmosViewMatchers
+import com.infinity.omos.util.SUCCESS_AUTH_CODE
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+
+@HiltAndroidTest
+class SignUpFragmentTest {
+
+    private val hiltRule = HiltAndroidRule(this)
+    private val activityTestRule = ActivityScenarioRule(OnboardingActivity::class.java)
+
+    @get:Rule
+    val rule: RuleChain = RuleChain
+        .outerRule(hiltRule)
+        .around(activityTestRule)
+
+    @Before
+    fun moveScreen() {
+        onView(withId(R.id.tv_sign_up)).perform(click())
+    }
+
+    @Test
+    fun `email_whenWrongEmail_showErrorMessage`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email"))
+        onView(withId(R.id.tv_send_auth_mail)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+
+        // then
+        onView(withId(R.id.ofv_email))
+            .check(ViewAssertions.matches(OmosViewMatchers.isDisplayedErrorMessage()))
+    }
+
+    @Test
+    fun `email_whenCorrectEmail_showAuthCodeField`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email@naver.com"))
+        onView(withId(R.id.tv_send_auth_mail)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+
+        // then
+        onView(withId(R.id.ofv_email_auth_code)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun `authCode_whenWrongAuthCode_showErrorMessage`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email@naver.com"))
+        onView(withId(R.id.tv_send_auth_mail)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+        onView(withId(R.id.ofv_email_auth_code)).perform(OmosViewActions.replaceText("000000"))
+
+        // then
+        onView(withId(R.id.ofv_email_auth_code)).check(ViewAssertions.matches(OmosViewMatchers.isDisplayedErrorMessage()))
+    }
+
+    @Test
+    fun `password_whenWrongPassword_showErrorMessage`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_password)).perform(OmosViewActions.replaceText("password"))
+        onView(withId(R.id.ofv_confirm_password)).perform(click())
+
+        // then
+        onView(withId(R.id.ofv_password)).check(ViewAssertions.matches(OmosViewMatchers.isDisplayedErrorMessage()))
+    }
+
+    @Test
+    fun `confirmPassword_whenNotMatchPassword_showErrorMessage`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_password)).perform(OmosViewActions.replaceText("password123!"))
+        onView(withId(R.id.ofv_confirm_password)).perform(OmosViewActions.replaceText("password123"))
+        onView(withId(R.id.ofv_password)).perform(click())
+
+        // then
+        onView(withId(R.id.ofv_confirm_password)).check(ViewAssertions.matches(OmosViewMatchers.isDisplayedErrorMessage()))
+    }
+
+    @Test
+    fun `completeButton_whenCorrectAuthCodeAndCorrectPassword_showSuccessMessageAndHideAuthCodeFieldAndActivateNextButton`() {
+        // given
+
+        // when
+        onView(withId(R.id.ofv_email)).perform(OmosViewActions.replaceText("email@naver.com"))
+        onView(withId(R.id.tv_send_auth_mail)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+        onView(withId(R.id.tv_ok)).perform(click())
+        onView(withId(R.id.ofv_email_auth_code)).perform(
+            OmosViewActions.replaceText(
+                SUCCESS_AUTH_CODE
+            )
+        )
+        onView(withId(R.id.ofv_password)).perform(OmosViewActions.replaceText("password123!"))
+        onView(withId(R.id.ofv_confirm_password)).perform(OmosViewActions.replaceText("password123!"))
+
+        // then
+        onView(withId(R.id.ofv_email)).check(ViewAssertions.matches(OmosViewMatchers.isDisplayedSuccessMessage()))
+        onView(withId(R.id.ofv_email_auth_code)).check(
+            ViewAssertions.matches(
+                Matchers.not(
+                    ViewMatchers.isDisplayed()
+                )
+            )
+        )
+        onView(withId(R.id.btn_next)).check(ViewAssertions.matches(ViewMatchers.isEnabled()))
+    }
+}
---
+++ app/src/test/java/com/infinity/omos/ui/onboarding/SignUpViewModelTest.kt
@@ -0,0 +1,214 @@
+package com.infinity.omos.ui.onboarding
+
+import com.google.common.truth.Truth.assertThat
+import com.infinity.omos.data.NetworkResult
+import com.infinity.omos.data.auth.AuthCode
+import com.infinity.omos.repository.auth.AuthRepository
+import com.infinity.omos.repository.user.UserRepository
+import com.infinity.omos.ui.MainDispatcherRule
+import com.infinity.omos.ui.onboarding.base.OnboardingState
+import com.infinity.omos.ui.onboarding.signup.SignUpViewModel
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SignUpViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: SignUpViewModel
+
+    @MockK
+    private lateinit var userRepository: UserRepository
+
+    @MockK
+    private lateinit var authRepository: AuthRepository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        viewModel = SignUpViewModel(userRepository, authRepository)
+    }
+
+    @Test
+    fun `이메일이 비어있을 때 에러 메시지 반환`() {
+        // given
+        val text = ""
+
+        // when
+        val result = viewModel.getEmailErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.BLANK_EMAIL_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `이메일 양식이 틀릴 때 에러 메시지 반환`() {
+        // given
+        val text = "email"
+
+        // when
+        val result = viewModel.getEmailErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.INCORRECT_CONTENTS_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `이메일 양식이 일치할 때 성공 메시지 반환`() {
+        // given
+        val text = "email@naver.com"
+
+        // when
+        val result = viewModel.getEmailErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NO_ERROR)
+    }
+
+    @Test
+    fun `인증코드가 일치하지 않을 때 에러 메시지 반환`() {
+        // given
+        val input = "123456"
+        val authCode = "000000"
+        viewModel.setAuthCode(authCode)
+
+        // when
+        val result = viewModel.getAuthCodeErrorMessage(input, 6)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.INCORRECT_AUTH_CODE_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `인증코드가 일치할 때 성공 메시지 반환`() {
+        // given
+        val input = "123456"
+        val authCode = "123456"
+        coEvery { authRepository.sendAuthMail(any()) } returns Result.success(AuthCode(authCode))
+        coEvery { userRepository.isNotEmailDuplicate(any()) } returns Result.success(NetworkResult(true))
+
+        // when
+        viewModel.sendAuthMail()
+        val result = viewModel.getAuthCodeErrorMessage(input, 6)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NO_ERROR)
+    }
+
+    @Test
+    fun `메일로 인증코드를 성공적으로 보냈을 때 Success 상태가 되는가`() {
+        // given
+        val authCode = "123456"
+        coEvery { authRepository.sendAuthMail(any()) } returns Result.success(AuthCode(authCode))
+        coEvery { userRepository.isNotEmailDuplicate(any()) } returns Result.success(NetworkResult(true))
+
+        // when
+        viewModel.sendAuthMail()
+        val result = viewModel.state.value
+
+        // then
+        assertThat(result).isInstanceOf(OnboardingState.Success::class.java)
+    }
+
+    @Test
+    fun `메일로 인증코드를 보내는 것을 실패했을 때 Failure 상태가 되는가`() {
+        // given
+        val exception = Exception()
+        coEvery { authRepository.sendAuthMail(any()) } returns Result.failure(exception)
+        coEvery { userRepository.isNotEmailDuplicate(any()) } returns Result.success(NetworkResult(true))
+
+        // when
+        viewModel.sendAuthMail()
+        val result = viewModel.state.value
+
+        // then
+        assertThat(result).isInstanceOf(OnboardingState.Failure::class.java)
+    }
+
+    @Test
+    fun `이미 가입된 이메일일 때 Failure 상태가 되는가`() {
+        // given
+        val authCode = "123456"
+        coEvery { authRepository.sendAuthMail(any()) } returns Result.success(AuthCode(authCode))
+        coEvery { userRepository.isNotEmailDuplicate(any()) } returns Result.success(NetworkResult(false))
+
+        // when
+        viewModel.sendAuthMail()
+        val result = viewModel.state.value
+
+        // then
+        assertThat(result).isInstanceOf(OnboardingState.Failure::class.java)
+    }
+
+    @Test
+    fun `비밀번호가 비어 있을 때 에러 메시지 반환`(){
+        // given
+        val text = ""
+
+        // when
+        val result = viewModel.getPasswordErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.BLANK_PASSWORD_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `비밀번호 양식이 일치하지 않을 때 에러 메시지 반환`() {
+        // given
+        val text = "password"
+
+        // when
+        val result = viewModel.getPasswordErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NOT_MATCH_PASSWORD_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `비밀번호 양식이 일치할 때 성공 메시지 반환`() {
+        // given
+        val text = "password123!"
+
+        // when
+        val result = viewModel.getPasswordErrorMessage(text)
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NO_ERROR)
+    }
+
+    @Test
+    fun `비밀번호와 비밀번호 재확인이 동일하지 않을 때 에러 메시지 반환`() {
+        // given
+        val password = "aaaa"
+        val confirmPassword = "bbbb"
+        viewModel.setPassword(password)
+        viewModel.setConfirmPassword(confirmPassword)
+
+        // when
+        val result = viewModel.getConfirmPasswordErrorMessage()
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NOT_MATCH_CONFIRM_PASSWORD_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `비밀번호와 비밀번호 재확인이 동일할 때 성공 메시지 반환`() {
+        val password = "aaaa"
+        val confirmPassword = "aaaa"
+        viewModel.setPassword(password)
+        viewModel.setConfirmPassword(confirmPassword)
+
+        // when
+        val result = viewModel.getConfirmPasswordErrorMessage()
+
+        // then
+        assertThat(result).isEqualTo(ErrorMessage.NO_ERROR)
+    }
+}
