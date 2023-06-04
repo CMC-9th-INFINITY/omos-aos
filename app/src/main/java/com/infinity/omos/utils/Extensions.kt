f90bb329cde0bc0b8323cf300225f40ace8c6010
jaemin-Yoo <iamjm29@naver.com>
2023-06-04 17:18:00 +0900

    Feat: 풀스크린 적용
    - BottomNavigation이 표시되는 모든 Fragment에만 Full Screen 적용

--- app/src/main/java/com/infinity/omos/utils/Extensions.kt
+++ app/src/main/java/com/infinity/omos/utils/Extensions.kt
@@ -1,5 +1,8 @@
 package com.infinity.omos.utils
 
+import android.app.Activity
+import android.os.Build
+import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
@@ -10,5 +13,21 @@ import kotlinx.coroutines.launch
 fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
     lifecycleScope.launch {
         this@repeatOnStarted.repeatOnLifecycle(Lifecycle.State.STARTED, block)
+    }
+}
+
+fun Activity.onFullScreen() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.setDecorFitsSystemWindows(false)
+    } else {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    }
+}
+
+fun Activity.offFullScreen() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.setDecorFitsSystemWindows(true)
+    } else {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
     }
 }
